851b7b3fd21768f8964fb7170545a87beedcf45a
Raphael <contato.raphaelinacio@gmail.com>
2017-08-25 18:46:24 -0300

    delete implementação user

--- feed-rest-api/src/main/java/br/com/infoglobo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.infoglobo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import br.com.infoglobo.domain.User;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
--- feed-rest-api/src/main/java/br/com/infoglobo/security/SecurityConfig.java
+++ feed-rest-api/src/main/java/br/com/infoglobo/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package br.com.infoglobo.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,19 +7,12 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Component;
 
-import br.com.infoglobo.service.UserDetailServiceImpl;
-
 @Configuration
 @Component
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
-	@Autowired
-	private UserDetailServiceImpl userDetailsService;
 	
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		//auth.userDetailsService(userDetailsService);
-		
 		auth
 		.inMemoryAuthentication()
 		.withUser("infoglobo")
